5e9a40bdd13bd343cbcb81608f2aab0af2554641
Mario Ortiz <mario.ortiz@jakala.com>
2025-04-09 14:05:31 +0200

    added tests

---
+++ src/App.test.tsx
@@ -0,0 +1,154 @@
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { App } from './App';
+import { beforeEach, describe, expect, Mock, test, vi } from 'vitest';
+
+const mockData = {
+  results: [
+    { url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+    { url: 'https://pokeapi.co/api/v2/pokemon/2/' },
+  ],
+};
+
+vi.mock('node-fetch', () => ({
+  default: vi.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockData),
+    })
+  ),
+}));
+globalThis.fetch = vi.fn() as Mock;
+
+const pokemonData = [
+  {
+    id: 1,
+    name: 'bulbasaur',
+    types: [{ type: { name: 'grass' } }],
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default:
+            'https://img.pokemondb.net/artwork/large/bulbasaur.jpg',
+        },
+      },
+    },
+    stats: [
+      { base_stat: 45 },
+      { base_stat: 49 },
+      { base_stat: 49 },
+      { base_stat: 65 },
+      { base_stat: 65 },
+      { base_stat: 45 },
+    ],
+  },
+  {
+    id: 2,
+    name: 'ivysaur',
+    types: [{ type: { name: 'grass' } }],
+    sprites: {
+      other: {
+        'official-artwork': {
+          front_default: 'https://img.pokemondb.net/artwork/large/ivysaur.jpg',
+        },
+      },
+    },
+    stats: [
+      { base_stat: 60 },
+      { base_stat: 62 },
+      { base_stat: 63 },
+      { base_stat: 80 },
+      { base_stat: 80 },
+      { base_stat: 60 },
+    ],
+  },
+];
+
+describe('App Component', () => {
+  beforeEach(() => {
+    // Mock the first API response (Pokemon list)
+    (fetch as Mock).mockResolvedValueOnce({
+      json: () => Promise.resolve(mockData),
+    });
+
+    // Mock the second API response (individual Pokemon details)
+    (fetch as Mock).mockResolvedValueOnce({
+      json: () => Promise.resolve(pokemonData[0]),
+    });
+
+    (fetch as Mock).mockResolvedValueOnce({
+      json: () => Promise.resolve(pokemonData[1]),
+    });
+  });
+
+  test('renders Pokémon search input', () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(/search a pokémon.../i);
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  test('displays loading state while fetching data', async () => {
+    render(<App />);
+
+    expect(screen.getByText('Pokédex')).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.queryByText('No results')).toBeInTheDocument();
+    });
+  });
+
+  test('renders Pokémon data after fetch', async () => {
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText('bulbasaur')).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.queryByText('No results')).not.toBeInTheDocument();
+    });
+  });
+
+  test('filters Pokémon data based on search query', async () => {
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.queryByText('No results')).not.toBeInTheDocument();
+    });
+
+    fireEvent.change(screen.getByPlaceholderText('Search a Pokémon...'), {
+      target: { value: 'bul' },
+    });
+
+    expect(screen.getByText('bulbasaur')).toBeInTheDocument();
+
+    expect(screen.queryByText('ivysaur')).not.toBeInTheDocument();
+  });
+
+  test("displays 'No results' when no Pokémon match the search", async () => {
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.queryByText('No results')).not.toBeInTheDocument();
+    });
+
+    fireEvent.change(screen.getByPlaceholderText('Search a Pokémon...'), {
+      target: { value: 'xyz' },
+    });
+
+    expect(screen.getByText('No results')).toBeInTheDocument();
+  });
+
+  //   test('handles region dropdown interaction', async () => {
+  //     render(<App />);
+
+  //     expect(screen.getByRole('button')).toHaveTextContent('kanto');
+
+  //     fireEvent.click(screen.getByRole('button'));
+
+  //     expect(screen.getByText('johto')).toBeInTheDocument();
+
+  //     fireEvent.click(screen.getByText('johto'));
+
+  //     expect(screen.getByRole('button')).toHaveTextContent('johto');
+  //   });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgrPlugin from "vite-plugin-svgr";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
