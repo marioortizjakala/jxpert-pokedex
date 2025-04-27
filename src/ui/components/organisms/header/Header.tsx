ab757e5ddaccb71fba54629edaf91596282ff2ae
marioortizjakala <mario.ortiz@jakala.com>
2025-04-27 17:12:05 +0200

    header fixes

--- src/ui/components/organisms/header/Header.tsx
+++ src/ui/components/organisms/header/Header.tsx
@@ -4,6 +4,12 @@ const HeaderStyled = styled.header`
   display: flex;
   background-color: ${({ theme }) => theme.colors.background0};
   box-shadow: ${({ theme }) => theme.shadows.m};
+  align-items: center;
+
+  & img {
+    margin: ${({ theme }) => theme.spacing['2xs']};
+    width: ${({ theme }) => theme.spacing['2xl']};
+  }
 `;
 
 const Header = ({ title }) => (
